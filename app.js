d04c5f3f6e49b81bb90d0240551c4673e1de1740
Jessica Benson <jess.nicole.benson@gmail.com>
2023-06-04 23:24:34 -0400

    update

--- app.js
+++ app.js
@@ -1,17 +1,49 @@
 require('dotenv').config()
 const express = require('express')
 const app = express()
-// TODO: import the getCityInfo and getJobs functions from util.js
+app.use(express.json())
+const util = require('./util')
+var cityInfo
+var jobs
 
-// TODO: Statically serve the public folder
+app.use(express.static('public'))
+///////////
+let getJobs = async() => {
+    jobs = await util.getJobs;
+}
 
-// TODO: declare the GET route /api/city/:city
-// This endpoint should call getCityInfo and getJobs and return
-// the result as JSON.
-// The returned JSON object should have two keys:
-// cityInfo (with value of the getCityInfo function)
-// jobs (with value of the getJobs function)
-// If no city info or jobs are found,
-// the endpoint should return a 404 status
+let getCityInfo = async() => {
+    cityInfo = await util.getCityInfo;
+}
+////////
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'))
+})
 
+app.get('/index.js', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.js'))
+})
+
+app.get('/style.css', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/style.css'))
+})
+
+app.get('/api/city/:city', async (req, res) => {
+    try {
+        var cityInfo = await util.getCityInfo(req.params.city)
+        var jobs = await util.getJobs(req.params.city)
+        if (!cityInfo && !jobs){
+            throw new Error
+        }
+        return res.status(200).json({
+            cityInfo: cityInfo,
+            jobs: jobs
+        })
+    }catch (err){
+        res.status(404).json({
+            error: err
+        })
+    }
+    }
+)
 module.exports = app
